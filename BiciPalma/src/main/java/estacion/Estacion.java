1853cab225c7c8ad21f76d15a71c1876731a1796
gboned <gboned@cifpfbmoll.eu>
2020-02-13 18:58:24 +0100

    feat: Añadido a clase Estacion atributo anclajes

--- BiciPalma/src/main/java/estacion/Estacion.java
+++ BiciPalma/src/main/java/estacion/Estacion.java
@@ -5,6 +5,7 @@ public class Estacion {
 	private int id;
 	private String direccion;
 	private int numerosAnclajes;
+	private int[] anclajes = new int[numerosAnclajes];
 	// Creo el constructor de la clase, a lo que se accede cuando se crea un nuevo
 	// objeto de la clase Estacion
 	public Estacion(int id, String direccion, int numerosAnclajes) {
@@ -12,6 +13,7 @@ public class Estacion {
 		this.direccion = direccion;
 		this.numerosAnclajes = numerosAnclajes;
 	}
+
 	// Creo el siguiente método para indicar lo que se tiene que devolver
 	// cuando se quiere consultar la estación
 	public String consultarEstacion() {
