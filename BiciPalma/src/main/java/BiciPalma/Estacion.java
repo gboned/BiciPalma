210ca576b239e2785f9dce3606dc2ff6de2ea6f2
gboned <gboned@cifpfbmoll.eu>
2020-02-13 16:04:03 +0100

    Comentarios añadidos a método consultarEstacion y a su test

--- BiciPalma/src/main/java/BiciPalma/Estacion.java
+++ BiciPalma/src/main/java/BiciPalma/Estacion.java
@@ -1,17 +1,19 @@
 package BiciPalma;
 
 public class Estacion {
-	
+	// Creo los atributos de la clase
 	private int id;
 	private String direccion;
 	private int numerosAnclajes;
-	
+	// Creo el constructor de la clase, a lo que se accede cuando se crea un nuevo
+	// objeto de la clase Estacion
 	public Estacion(int id, String direccion, int numerosAnclajes) {
 		this.id = id;
 		this.direccion = direccion;
 		this.numerosAnclajes = numerosAnclajes;
 	}
-	
+	// Creo el siguiente método para indicar lo que se tiene que devolver
+	// cuando se quiere consultar la estación
 	public String consultarEstacion() {
 		return "id: " + id + "\ndireccion: " + direccion + "\nnumerosAnclajes: " + numerosAnclajes;
 		
--- BiciPalma/src/test/java/BiciPalma/EstacionTest.java
+++ BiciPalma/src/test/java/BiciPalma/EstacionTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 public class EstacionTest {
 
 	@Test
+	// En este test se comprueba que cuando se crea un nuevo objeto de la clase
+	// Estacion, lo devuelva de la forma que se espera
 	public void consultarEstacionTest() {
 		Estacion estacion = new Estacion(1, "Manacor", 6);
 		assertEquals("id: 1\ndireccion: Manacor\nnumerosAnclajes: 6", estacion.consultarEstacion());
